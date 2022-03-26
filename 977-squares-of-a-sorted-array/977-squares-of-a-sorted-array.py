7c68fa6575495e97a4a68d6cc3587941a9c3cd01
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-03-26 15:47:48 +0530

    Time: 304 ms (55.27%), Space: 16.3 MB (52.02%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,8 +1,22 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:    
-        res=[]
-        for el in nums:
-            res.append(el**2)
-        res.sort()
-        return res
+        """
+        intuition: el to be present at the end of the resultant array would be either the square of the first no or the last.
+        we will keep two pointers, one pointing at index 0 and other at index n-1. And pushing the larger el at the end of res arr.
         
+        TC: O(n)
+        SC: O(n)
+        
+        """
+        n=len(nums)
+        res=[0]*n
+        left,right=0,n-1
+        
+        for i in range(n-1,-1,-1):
+            if abs(nums[left])<abs(nums[right]):
+                res[i]=nums[right]**2
+                right-=1
+            else:
+                res[i]=nums[left]**2
+                left+=1
+        return res
