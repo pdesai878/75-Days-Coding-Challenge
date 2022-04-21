bc162231dd23db459df223b1246a2775d763fd3d
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-04-21 22:16:43 +0530

    Time: 130 ms (14.35%), Space: 14 MB (46.15%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -9,22 +9,11 @@ class Solution:
         carry=0
         s=0
         while l1 or l2:
-            
-            if l1 and l2:
-                s=l1.val+l2.val+carry
-                carry=s//10
-                dummy.next=ListNode(s%10)
-            elif l1:
-                s=l1.val+carry
-                carry=s//10
-                dummy.next=ListNode(s%10)
-            elif l2:
-                s=l2.val+carry
-                carry=s//10
-                dummy.next=ListNode(s%10)
-            else:
-                break
-            # print(dummy.val)
+            x=l1.val if l1 else 0
+            y=l2.val if l2 else 0
+            s=x+y+carry
+            carry=s//10
+            dummy.next=ListNode(s%10)
             dummy=dummy.next
             if l1:
                 l1=l1.next
