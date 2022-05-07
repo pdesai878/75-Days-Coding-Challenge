445c51222313ba7bc88d85ee456184ebee16f09e
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-05-07 14:24:42 +0530

    Time: 80 ms (9.08%), Space: 14.1 MB (93.96%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,11 +1,10 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         def gen_subsets(res,ind):
-            if ind>=n:
-                s.add(tuple(res))
-                return
-            gen_subsets(res+[nums[ind]],ind+1)
-            gen_subsets(res,ind+1)
+            s.add(tuple(res))
+            for i in range(ind,n):
+                gen_subsets(res+[nums[i]],i+1)
+            
             
         s=set()
         nums.sort()
