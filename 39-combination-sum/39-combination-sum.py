eac2f676cdc2127d9f192ac140ff50f5de60ed1e
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-05-07 22:48:39 +0530

    Time: 79 ms (82.65%), Space: 13.9 MB (91.79%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def get_combo(temp,s,ind):
+            if s>target:
+                return 
+            if s==target:
+                res.append(temp)
+                return
+                
+            for i in range(ind,n):
+                get_combo(temp+[candidates[i]],s+candidates[i],i)
+        res=[]
+        n=len(candidates)
+        candidates.sort()
+        get_combo([],0,0)
+        return res
+                
+        
