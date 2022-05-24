2d423c6b2309c9d479fd263e2f184fb58073b970
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-05-24 10:05:05 +0530

    Time: 67 ms (25.94%), Space: 14.1 MB (22.58%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,22 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def solve(res):
-            if len(res)==n:
-                ans.append(res)
+        def solve(ind):
+            if ind==n:
+                ans.append(nums[::])
                 return
-                
-            for i in range(n):
-                if visited[i]:
-                    continue
-                visited[i]=True
-                solve(res+[nums[i]])
-                visited[i]=False
-                
-                
+               
+            for i in range(ind,n):
+                nums[ind],nums[i]=nums[i],nums[ind]
+                solve(ind+1)
+                nums[ind],nums[i]=nums[i],nums[ind]
+                            
         ans=[]
         n=len(nums)
-        visited=[False for i in range(n)]
-        solve([])
+        solve(0)
         return ans
                 
         
