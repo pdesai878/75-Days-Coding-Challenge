da1648749ce78a4b877745758204ca38ec3dd943
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-05-25 17:16:31 +0530

    Time: 1048 ms (21.59%), Space: 30.1 MB (70.65%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,25 +1,26 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        def isPalindrome(i,j):
+        def isPalindrome(string):
+            i=0
+            j=len(string)-1
             while i<=j:
-                if s[i]!=s[j]:
+                if string[i]!=string[j]:
                     return False
                 i+=1
                 j-=1
             return True
         
-        def get_partitions(temp,ind):
+        def solve(ind,temp):
             if ind==n:
                 res.append(temp)
                 return
-            
             for i in range(ind,n):
-                if isPalindrome(ind,i):
-                    get_partitions(temp+[s[ind:i+1]],i+1)
+                if isPalindrome(s[ind:i+1]):
+                    solve(i+1,temp+[s[ind:i+1]])
                     
-        res=[]
         n=len(s)
-        get_partitions([],0)
+        res=[]
+        solve(0,[])
         return res
                 
                 
