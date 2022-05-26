72ffdbc1d98390be69b933ae2ffe500f354e67bd
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-05-26 22:47:52 +0530

    Added solution - LeetHub

---
+++ Find Nth root of M - GFG/find-nth-root-of-m.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+
+class Solution:
+	def NthRoot(self, n, m):
+	    if m==1 or m==0:
+	        return 1
+	    if n==1:
+	        return m
+		l=1
+		r=m//2
+		while l<=r:
+		    mid=l+(r-l)//2
+		    sq=mid**n
+		    if sq==m:
+		        return mid
+		    if sq<m:
+		        l=mid+1
+		    else:
+		        r=mid-1
+        return -1
+		    
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n, m = input().split()
+		n = int(n); m = int(m);
+		ob = Solution()
+		ans = ob.NthRoot(n, m)
+		print(ans)
+# } Driver Code Ends
