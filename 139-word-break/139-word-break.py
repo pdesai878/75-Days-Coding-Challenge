55394676eb75cbbbcba31e7c597e82b0ff099334
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-05-26 10:01:52 +0530

    Time: 51 ms (58.99%), Space: 14.2 MB (28.05%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,16 +1,22 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        @lru_cache(None)
+        
         def solve(ind):
             if ind==n:
                 return True
+            if dp[ind]!=-1:
+                return dp[ind]
+            
+            dp[ind]=False
             for i in range(ind,n):
                 if s[ind:i+1] in wordDict:
                     if solve(i+1):
-                        return True
-            return False
+                        dp[ind]=True
+                        break
+            return dp[ind]
                     
         n=len(s)
+        dp=[-1 for i in range(n)]
         return solve(0)
                     
         
