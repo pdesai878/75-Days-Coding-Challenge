14f26b3b0a5cf18ead51260fb1185804956da814
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-05-27 17:04:40 +0530

    Time: 281 ms (25.06%), Space: 23.8 MB (13.22%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -1,17 +1,25 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         n=len(nums)
+        
+        #boundary checks
         if n==1:
             return nums[0]
-        left=0
-        right=n-1
-        while left<right:
-            mid=left+(right-left)//2
+        if nums[0]!=nums[1]:
+            return nums[0]
+        if nums[-1]!=nums[-2]:
+            return nums[-1]
+        
+        l=0
+        r=n-1
+        while l<=r:
+            mid=l+(r-l)//2
+            if nums[mid]!=nums[mid-1] and nums[mid]!=nums[mid+1]:
+                return nums[mid]
             if (mid&1 and nums[mid]==nums[mid-1]) or (mid&1==0 and nums[mid]==nums[mid+1]):
-                left=mid+1
+                l=mid+1
             else:
-                right=mid
-        return nums[left]
-            
+                r=mid-1
+        return -1
         
         
