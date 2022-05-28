dbd9573a4c3bb13259f253ed8339e1b0d15c1c98
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-05-28 09:53:26 +0530

    Time: 203 ms (43.38%), Space: 15.2 MB (77.27%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -2,7 +2,12 @@ class Solution:
     def missingNumber(self, arr: List[int]) -> int:
         #bit manipulation
         n=len(arr)
-        missing_no=n
-        for ind,el in enumerate(arr):
-            missing_no^=ind^el
-        return missing_no
+        # missing_no=n
+        # for ind,el in enumerate(arr):
+        #     missing_no^=ind^el
+        # return missing_no
+    
+        #gauss formula
+        expected_sum=n*(n+1)//2
+        actual_sum=sum(arr)
+        return expected_sum-actual_sum
