d17cb2460675b7c9a3e52633e430b07362fc4444
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-05-29 11:38:03 +0530

    Time: 379 ms (90.67%), Space: 14.4 MB (71.68%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -2,25 +2,22 @@ class Solution:
     def maxProduct(self, words: List[str]) -> int:
         
         n=len(words)
-        mask=[0]*n
-        length=[0]*n
-        bit=lambda xh: ord(ch)-ord('a')
-        ind=0
+        hashmap=defaultdict(int)
+        bit=lambda ch: ord(ch)-ord('a')
+        
         for word in words:
             bitmask=0
             l=0
             for ch in word:
                 bitmask|=1<<bit(ch)
                 l+=1
-            mask[ind]=bitmask
-            length[ind]=l
-            ind+=1
+            hashmap[bitmask]=max(hashmap[bitmask],l)
+           
         mx=0
-        for i in range(n-1):
-            for j in range(i+1,n):
-                
-                if mask[i]&mask[j]:
+        for x in hashmap:
+            for y in hashmap:
+                if x&y:
                     continue
-                mx=max(mx,length[i]*length[j])
+                mx=max(mx,hashmap[x]*hashmap[y])
         return mx
         
