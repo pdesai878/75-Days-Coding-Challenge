b7343c5ddbaac5b9cbb5eddef2d96eeaa32ebbe8
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-05-31 08:25:43 +0530

    Time: 58 ms (46.66%), Space: 14.2 MB (56.90%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,38 +1,26 @@
 class Solution:
     def search(self, arr: List[int], target: int) -> int:
-        def bs(l,r,key):
-            
-            while l<=r:
-                mid=l+(r-l)//2
-                if arr[mid]==key:
-                    return mid
-                if arr[mid]<key:
-                    l=mid+1
-                else:
-                    r=mid-1
-            return -1
-        
-        def find_pivot():
-            l=0
-            r=n-1
-            pivot=-1
-            while l<=r:
-                mid=l+(r-l)//2
-                if arr[0]<=arr[mid]:
-                    pivot=mid
-                    l=mid+1
-                else:
-                    r=mid-1
-            return pivot
-                    
-                    
-         
         n=len(arr)
-        pivot=find_pivot()
-        bs1=bs(0,pivot,target)
-        if bs1!=-1:
-            return bs1
-        return bs(pivot+1,n-1,target)
+        l=0
+        r=n-1
+        while l<=r:
+            mid=l+(r-l)//2
+            if arr[mid]==target:
+                return mid
+            if arr[mid]>=arr[l]:
+                if target>=arr[l] and target<arr[mid]:
+                    r=mid-1
+                else:
+                    l=mid+1
+            else:
+                if target<=arr[r] and target>arr[mid]:
+                    l=mid+1
+                else:
+                    r=mid-1
+        return -1
+                
+        
+        
            
         
             
