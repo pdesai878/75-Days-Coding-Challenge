786acaa863c0316553271a4db78da685aa49486f
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-06-12 11:42:49 +0530

    Time: 3679 ms (55.56%), Space: 14.5 MB (33.33%) - LeetHub

---
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.py
@@ -0,0 +1,24 @@
+class Solution:
+    def matchReplacement(self, s: str, sub: str, mappings: List[List[str]]) -> bool:
+        def check(pos):
+            if pos+k>n:
+                return False
+            ind=0
+            for i in range(pos,pos+k):
+                if s[i]==sub[ind] or (sub[ind] in dicti and s[i] in dicti[sub[ind]]):
+                    ind+=1
+                else:
+                    return False
+            return True
+        
+        k=len(sub)
+        n=len(s)
+        dicti=defaultdict(set)
+        for ch,re in mappings:
+            dicti[ch].add(re)
+        for i in range(n):
+            if check(i):
+                return True
+        return False
+            
+        
