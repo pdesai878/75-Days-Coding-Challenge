67099774ec0fd30dfa169855d531ce19f60bed70
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-08 13:55:49 +0530

    Time: 44 ms (93.38%), Space: 14 MB (43.32%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -4,29 +4,31 @@ class Solution:
         col=len(grid[0])
         if grid[0][0]==1 or grid[-1][-1]==1:
             return 0
-        dp=[[0 for j in range(col)] for i in range(2)]
+        prev=[0]*col
         
         for i in range(row):
+            curr=[0]*col
             for j in range(col):
                 if i>=0 and j>=0 and grid[i][j]==1:
-                    dp[i&1][j]=0
+                    curr[j]=0
                     continue
                     
                 if i==0 and j==0:
-                    dp[i&1][j]=1
+                    curr[j]=1
                     continue
                         
                 up=left=0
                 
                 if i>0: 
-                    up = dp[(i-1)&1][j]
+                    up = prev[j]
                 if j>0:
-                    left = dp[i&1][j-1]
+                    left = curr[j-1]
                     
-                dp[i&1][j]=up+left
+                curr[j]=up+left
+            prev=curr
                 
-        print(dp)
-        return dp[i&1][-1]
+       
+        return curr[-1]
                 
         
         
