a6600faedde151b044fd052d5f2d6027aa59dc1b
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-08 17:36:07 +0530

    Time: 140 ms (17.60%), Space: 15 MB (62.84%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -3,15 +3,15 @@ class Solution:
         n=len(triangle)
         if n==1:
             return triangle[0][0]
-        dp=[[0 for j in range(n)] for i in range(2)]
-        for c in range(n):
-            dp[(n-1)&1][c]=triangle[n-1][c]
         
+        prev=triangle[n-1]
+    
         for i in range(n-2,-1,-1):
+            curr=[0]*n
             for j in range(i,-1,-1):
-                dp[i&1][j]=triangle[i][j]+min(dp[(i+1)&1][j],dp[(i+1)&1][j+1])
-           
-        return dp[i&1][0]
+                curr[j]=triangle[i][j]+min(prev[j],prev[j+1])
+            prev=curr
+        return curr[0]
         
        
             
