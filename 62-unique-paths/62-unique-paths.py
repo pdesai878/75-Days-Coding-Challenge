b09a91c85f9dff996c0c90449a3bc7ed9aafa4ce
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-08 13:26:51 +0530

    Time: 31 ms (95.16%), Space: 13.9 MB (74.96%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -4,15 +4,24 @@ class Solution:
         if m==1 or n==1:
             return 1
         #dp initialisation
-        dp=[[0 for j in range(n)] for i in range(2)]
-        for col in range(n):
-            dp[0][col]=1
-        for row in range(2):
-            dp[row][0]=1
-        for i in range(1,m):
-            for j in range(1,n):
-                dp[i&1][j]=dp[(i-1)&1][j]+dp[i&1][j-1]
-        return dp[i&1][-1]
+        prev=[1]*n
+        
+        for i in range(m):
+            curr=[0]*n
+            for j in range(n):
+                if i==0 and j==0:
+                    curr[j]=1
+                    continue
+                up=left=0
+                if i>0:
+                    up=prev[j]
+                if j>0:
+                    left=curr[j-1]
+                curr[j]=up+left
+                    
+            prev=curr
+            
+        return curr[-1]
         
         
             
