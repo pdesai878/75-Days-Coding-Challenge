2746254ae142e45ce59b46c3ac993f88c06fbad8
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-09 08:32:07 +0530

    Time: 331 ms (12.80%), Space: 14.7 MB (89.69%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -4,22 +4,20 @@ class Solution:
         if n==1:
             return matrix[0][0]
         mn=sys.maxsize
-        dp=[[0 for j in range(n)]for i in range(2)]
+        prev=matrix[-1]
         
-        for i in range(n-1,-1,-1):
+        for i in range(n-2,-1,-1):
+            curr=[0]*n
             for j in range(n):
-                if i==n-1:
-                    dp[i&1][j]=matrix[i][j]
-                    continue
                 if j==0:
-                    dp[i&1][j]=matrix[i][j]+min(dp[(i+1)&1][j],dp[(i+1)&1][j+1])
+                    curr[j]=matrix[i][j]+min(prev[j],prev[j+1])
                 elif j==n-1:
-                    dp[i&1][j]=matrix[i][j]+min(dp[(i+1)&1][j],dp[(i+1)&1][j-1])
+                    curr[j]=matrix[i][j]+min(prev[j],prev[j-1])
                 else:
-                    dp[i&1][j]=matrix[i][j]+min(dp[(i+1)&1][j],dp[(i+1)&1][j-1],dp[(i+1)&1][j+1])
+                    curr[j]=matrix[i][j]+min(prev[j],prev[j-1],prev[j+1])
+            prev=curr
                     
-        
-        # print(dp)
-        return min(dp[i&1])
+    
+        return min(curr)
         
         
