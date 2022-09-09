176e06982a05fc0caf99b4a73d13027c4d66bc4c
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-09 10:45:24 +0530

    Time: 3539 ms (26.80%), Space: 14.1 MB (86.55%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -14,17 +14,18 @@ class Solution:
         n=len(nums)
         if n==1:
             return nums[0]==target
-        dp=[[False for j in range(target+1)]for i in range(2)] 
-        for row in range(2):
-            dp[row][0]=True
-        
+        prev=[False for j in range(target+1)] 
+        prev[0]=True
         for i in range(1,n):
+            curr=[False for j in range(target+1)] 
+            curr[0]=True
             for j in range(1,target+1):
                 if nums[i]<=j:
-                    dp[i&1][j]=dp[(i-1)&1][j-nums[i]] or dp[(i-1)&1][j]
+                    curr[j]=prev[j-nums[i]] or prev[j]
                 else:
-                    dp[i&1][j]=dp[(i-1)&1][j]
-        return dp[i&1][-1]
+                    curr[j]=prev[j]
+            prev=curr
+        return curr[-1]
                 
                 
        
