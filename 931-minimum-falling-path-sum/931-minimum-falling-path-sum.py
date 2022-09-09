918044ad4f97999a0a51304ebd3bcb0fe735f392
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-09 08:29:26 +0530

    Time: 407 ms (5.03%), Space: 14.6 MB (89.69%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -4,22 +4,22 @@ class Solution:
         if n==1:
             return matrix[0][0]
         mn=sys.maxsize
-        dp=[[0 for j in range(n)]for i in range(n)]
+        dp=[[0 for j in range(n)]for i in range(2)]
         
         for i in range(n-1,-1,-1):
             for j in range(n):
                 if i==n-1:
-                    dp[i][j]=matrix[i][j]
+                    dp[i&1][j]=matrix[i][j]
                     continue
                 if j==0:
-                    dp[i][j]=matrix[i][j]+min(dp[i+1][j],dp[i+1][j+1])
+                    dp[i&1][j]=matrix[i][j]+min(dp[(i+1)&1][j],dp[(i+1)&1][j+1])
                 elif j==n-1:
-                    dp[i][j]=matrix[i][j]+min(dp[i+1][j],dp[i+1][j-1])
+                    dp[i&1][j]=matrix[i][j]+min(dp[(i+1)&1][j],dp[(i+1)&1][j-1])
                 else:
-                    dp[i][j]=matrix[i][j]+min(dp[i+1][j],dp[i+1][j-1],dp[i+1][j+1])
+                    dp[i&1][j]=matrix[i][j]+min(dp[(i+1)&1][j],dp[(i+1)&1][j-1],dp[(i+1)&1][j+1])
                     
         
         # print(dp)
-        return min(dp[0])
+        return min(dp[i&1])
         
         
