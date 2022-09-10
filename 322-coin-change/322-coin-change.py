4479fd2a8f13868f4336f18f87b24e6bfd63bb82
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-10 11:39:59 +0530

    Time: 7802 ms (5.00%), Space: 15.3 MB (28.36%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -3,20 +3,21 @@ class Solution:
         n=len(coins)
         dp=[[sys.maxsize for j in range(amount+1)] for i in range(n)]
         
+        prev=[sys.maxsize for j in range(amount+1)]
         for c in range(1,amount+1):
             if c%coins[0]==0:
-                dp[0][c]=c//coins[0]
-                
+                prev[c]=c//coins[0]
+        prev[0]=0      
         for i in range(n):
-            for j in range(amount+1):
-                if j==0:
-                    dp[i][j]=0
-                    continue
+            curr=[sys.maxsize for j in range(amount+1)]
+            curr[0]=0
+            for j in range(1,amount+1):
                 if coins[i]<=j:
-                    dp[i][j]=min(1+dp[i][j-coins[i]], dp[i-1][j])
+                    curr[j]=min(1+curr[j-coins[i]], prev[j])
                 else:
-                    dp[i][j]=dp[i-1][j]
-        return dp[-1][-1] if dp[-1][-1]!=sys.maxsize else -1
+                    curr[j]=prev[j]
+            prev=curr
+        return curr[-1] if curr[-1]!=sys.maxsize else -1
                 
             
         
