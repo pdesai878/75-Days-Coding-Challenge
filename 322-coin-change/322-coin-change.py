84cadf5d7aef54c906da29b39ad9b0a5822fb2af
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-10 11:35:05 +0530

    Time: 7073 ms (5.00%), Space: 16.2 MB (26.14%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,25 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        def getMinCoins(ind,amount):
-            if ind==0:
-                if amount%coins[ind]==0:
-                    return amount//coins[ind]
-                return sys.maxsize
-            
-            if dp[ind][amount]!=-1:
-                return dp[ind][amount]
-            #not pick
-            op1=getMinCoins(ind-1,amount)
-            #pick
-            op2=sys.maxsize
-            if coins[ind]<=amount:
-                op2=1+getMinCoins(ind,amount-coins[ind])
-            dp[ind][amount]=min(op1,op2)
-            return dp[ind][amount]
-  
         n=len(coins)
-        dp=[[-1 for j in range(amount+1)] for i in range(n)]
-        res=getMinCoins(n-1,amount)
-        return res if res!=sys.maxsize else -1
+        dp=[[sys.maxsize for j in range(amount+1)] for i in range(n)]
+        
+        for c in range(1,amount+1):
+            if c%coins[0]==0:
+                dp[0][c]=c//coins[0]
+                
+        for i in range(n):
+            for j in range(amount+1):
+                if j==0:
+                    dp[i][j]=0
+                    continue
+                if coins[i]<=j:
+                    dp[i][j]=min(1+dp[i][j-coins[i]], dp[i-1][j])
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[-1][-1] if dp[-1][-1]!=sys.maxsize else -1
+                
+            
+        
             
         
