5900e4d5247f3ebe4b118cc4d3795fdd96f635b2
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-13 23:56:37 +0530

    Time: 1249 ms (21.34%), Space: 29.9 MB (35.09%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -1,26 +1,41 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        def getWays(ind,target):
-            if target==0:
-                return 1
-            if ind==0:
-                if target%coins[ind]==0:
-                    return 1
-                return 0
-            if dp[ind][target]!=-1:
-                return dp[ind][target]
-            #not take
-            nt=getWays(ind-1,target)
-            #take
-            t=0
-            if coins[ind]<=target:
-                t=getWays(ind,target-coins[ind])
-            dp[ind][target]=t+nt
-            return dp[ind][target]
+        # def getWays(ind,target):
+        #     if target==0:
+        #         return 1
+        #     if ind==0:
+        #         if target%coins[ind]==0:
+        #             return 1
+        #         return 0
+        #     if dp[ind][target]!=-1:
+        #         return dp[ind][target]
+        #     #not take
+        #     nt=getWays(ind-1,target)
+        #     #take
+        #     t=0
+        #     if coins[ind]<=target:
+        #         t=getWays(ind,target-coins[ind])
+        #     dp[ind][target]=t+nt
+        #     return dp[ind][target]
         
         n=len(coins)
-        dp=[[-1 for j in range(amount+1)] for i in range(n)]
-        return getWays(n-1,amount)
+        dp=[[0 for j in range(amount+1)] for i in range(n)]
+        for r in range(n):
+            dp[r][0]=1
+        for c in range(amount+1):
+            if c%coins[0]==0:
+                dp[0][c]=1
+        
+        for i in range(1,n):
+            for j in range(1,amount+1):
+                if coins[i]<=j:
+                    dp[i][j]=dp[i][j-coins[i]]+dp[i-1][j]
+                else:
+                    dp[i][j]=dp[i-1][j]
+        return dp[-1][-1]
+                    
+                
+        
             
             
         
