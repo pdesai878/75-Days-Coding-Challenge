4123164f24f815fcf5e431921a93333ee3492fdc
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-14 22:11:56 +0530

    Time: 182 ms (5.14%), Space: 15.3 MB (25.44%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -14,15 +14,15 @@ class Solution:
                 
   
         n=len(prices)
-        dp=[[0 for j in range(2)] for i in range(n+1)]
+        dp=[[0 for j in range(2)] for i in range(2)]
         for i in range(n-1,-1,-1):
             for j in range(2):
                 if j:
-                    dp[i][j]=max(-prices[i]+dp[i+1][0], dp[i+1][1])
+                    dp[i&1][j]=max(-prices[i]+dp[(i+1)&1][0], dp[(i+1)&1][1])
                 else:
-                    dp[i][j]=max(prices[i]+dp[i+1][1], dp[i+1][0])
+                    dp[i&1][j]=max(prices[i]+dp[(i+1)&1][1], dp[(i+1)&1][0])
         
-        return dp[0][1]
+        return dp[i&1][1]
                     
 
                     
