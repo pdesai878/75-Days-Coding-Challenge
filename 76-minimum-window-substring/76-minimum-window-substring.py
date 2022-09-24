cb3c0760597c4de24064e5a4babb9cc7fe5f9a4b
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-24 09:25:20 +0530

    Time: 336 ms (23.68%), Space: 14.7 MB (83.83%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -2,32 +2,31 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if s==t:
             return s
-        i=0
         dicti=Counter(t)
-        n=len(s)
-        mx=sys.maxsize
-        count=len(dicti)
         ans=""
+        count=len(dicti)
+        i=0
+        n=len(s)
+        mn=n+1
         for j in range(n):
             el=s[j]
             if el in dicti:
                 dicti[el]-=1
                 if dicti[el]==0:
                     count-=1
-            
+                    
             if count==0:
                 while count==0 and i<n:
-                    if j-i+1<mx:
-                        mx=j-i+1
+                    if j-i+1<mn:
+                        mn=j-i+1
                         ans=s[i:j+1]
                     if s[i] in dicti:
                         dicti[s[i]]+=1
                         if dicti[s[i]]==1:
                             count+=1
                     i+=1
-    
         return ans
-                        
             
+                
             
         
