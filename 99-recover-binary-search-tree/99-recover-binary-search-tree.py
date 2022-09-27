623d4388154d659eca519c5ef4019c05a39244f3
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-27 09:20:27 +0530

    Time: 136 ms (39.37%), Space: 14.3 MB (63.97%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.py
@@ -6,29 +6,27 @@
 #         self.right = right
 class Solution:
     def recoverTree(self, root: Optional[TreeNode]) -> None:
-        self.p=0
-        #brute force
-        def get_inorder(node):
+        def inorder(node):
             if not node:
                 return
-            get_inorder(node.left)
-            inorder.append(node.val)
-            get_inorder(node.right)
-            
-        def recover(node):
-            if not node:
-                return
-            recover(node.left)
-            if node.val!=inorder[self.p]:
-                node.val=inorder[self.p]
-            self.p+=1
-            recover(node.right)
-            
-        inorder=[]
-        get_inorder(root)
-        inorder.sort()
+            inorder(node.left)
+            if self.prev and node.val<self.prev.val:
+                if self.first is None:
+                    self.first=self.prev
+                    self.mid=node
+                else:
+                    self.last=node
+            self.prev=node
+            inorder(node.right)
+                
         
-        recover(root)
-        
+        self.prev=TreeNode(-sys.maxsize)
+        self.first=self.mid=self.last=None
+        inorder(root)
+        if self.first and self.last:
+            self.first.val,self.last.val=self.last.val,self.first.val
+        elif self.first and self.mid:
+            self.first.val,self.mid.val=self.mid.val,self.first.val
         return root
+            
         
