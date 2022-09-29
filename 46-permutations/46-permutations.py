31cf625417ce12402481e79f4ab1360e88d8734e
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-29 20:15:57 +0530

    Time: 101 ms (5.15%), Space: 14.2 MB (23.30%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,19 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def getPermutations(ind,temp): 
-            if len(temp)==n:
-                res.append(temp[::])
+        def getPermutations(ind): 
+            if ind==n:
+                res.append(nums[::])
                 return
-               
-            for i in range(n):
-                if i not in visited:
-                    visited.add(i) 
-                    getPermutations(i,temp+[nums[i]])
-                    visited.remove(i)
+            
+            for i in range(ind,n):
+                nums[i],nums[ind]=nums[ind],nums[i]
+                getPermutations(ind+1)
+                nums[i],nums[ind]=nums[ind],nums[i]
+                
+            
         
         res=[]
         n=len(nums)
-        visited=set()
-        getPermutations(0,[])
+        getPermutations(0)
         return res
        
