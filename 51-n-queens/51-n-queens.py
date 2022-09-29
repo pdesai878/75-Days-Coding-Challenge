efde8a2b4581821c9f519e379dacf8000ad94c51
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-29 20:33:18 +0530

    Time: 275 ms (15.61%), Space: 14.5 MB (47.13%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,46 +1,46 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def isPossible(row,col):
-            #check top-bottom
-            for i in range(row):
-                if visited[i][col]=="Q":
+        def isPossible(r,c):
+            #check col
+            for i in range(n):
+                if board[i][c]=="Q":
                     return False
-            #right diagonal
-            i=row
-            j=col
-            while i>=0 and j<n:
-                if visited[i][j]=="Q":
-                    return False
-                i-=1
-                j+=1
-            #left diagonal
-            i=row
-            j=col
+            
+            #check upper left diagonal
+            i=r
+            j=c
             while i>=0 and j>=0:
-                if visited[i][j]=="Q":
+                if board[i][j]=="Q":
                     return False
                 i-=1
                 j-=1
+            
+            #check upper right diagonal
+            i=r
+            j=c
+            while i>=0 and j<n:
+                if board[i][j]=="Q":
+                    return False
+                i-=1
+                j+=1
             return True
+                
             
-        
-        def solve(row):
-            if row==n:
+        def nqueens(r):
+            if r==n:
                 temp=[]
-                for r in visited:
-                    temp.append("".join(r))
-                    
+                for row in board:
+                    temp.append("".join(row))
                 res.append(temp)
-                      
             
-            for col in range(n):
-                if isPossible(row,col):
-                    visited[row][col]="Q"
-                    solve(row+1)
-                    visited[row][col]="."
-            
-        visited=[["." for col in range(n)] for row in range(n)]
+            for c in range(n):
+                if isPossible(r,c):
+                    board[r][c]="Q"
+                    nqueens(r+1)
+                    board[r][c]="."
+   
+         
         res=[]
-        solve(0)
+        board=[["." for j in range(n)] for i in range(n)]
+        nqueens(0)
         return res
-        
