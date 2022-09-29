137812ad42979eba89d20d7c96f49cc088baa36c
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-09-29 21:07:53 +0530

    Time: 677 ms (41.37%), Space: 13.8 MB (95.59%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.py
+++ 37-sudoku-solver/37-sudoku-solver.py
@@ -1,38 +1,35 @@
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
-        def isPossible(no,row,col):
-            for i in range(n):
-                #check in row
-                if board[row][i]==no:
+        def isPossible(r,c,no):
+            #check row & col
+            for i in range(9):
+                if board[i][c]==no:
                     return False
-                #check in col
-                if board[i][col]==no:
+                if board[r][i]==no:
                     return False
                 #check in grid
-                curr_row_in_grid=3*(row//3)+i//3
-                curr_col_in_grid=3*(col//3)+i%3
-                if board[curr_row_in_grid][curr_col_in_grid]==no:
+                rgrid=3*(r//3)+i//3
+                cgrid=3*(c//3)+i%3
+                if board[rgrid][cgrid]==no:
                     return False
             return True
-        
-        
+            
         def solve(board):
             for i in range(n):
                 for j in range(n):
-                    if board[i][j]==".":
+                    if board[i][j]=='.':
                         for no in range(1,10):
-                            number=str(no)
-                            if isPossible(number,i,j):
-                                board[i][j]=number
+                            if isPossible(i,j,str(no)):
+                                board[i][j]=str(no)
                                 done=solve(board)
                                 if done:
                                     return True
-                                else:
-                                    board[i][j]="."
+                                board[i][j]='.'
                         return False
             return True
-        
+                    
         n=9
         solve(board)
         return board
+        
         
