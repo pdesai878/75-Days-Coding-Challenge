833ed8850a5d1ae094377b89d638fd8d8343658d
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-10-01 18:12:35 +0530

    Time: 49 ms (85.08%), Space: 14.2 MB (55.55%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,31 +1,35 @@
 class Solution:
-    def search(self, arr: List[int], target: int) -> int:
-        n=len(arr)
-        l=0
-        r=n-1
-        while l<=r:
-            mid=l+(r-l)//2
-            if arr[mid]==target:
-                return mid
-            if arr[mid]>=arr[l]:
-                if target>=arr[l] and target<arr[mid]:
-                    r=mid-1
-                else:
-                    l=mid+1
-            else:
-                if target<=arr[r] and target>arr[mid]:
+    def search(self, nums: List[int], target: int) -> int:
+        def findPeak(l,r):
+            pivot=-1
+            while l<=r:
+                mid=l+(r-l)//2
+                if nums[mid]>=nums[0]:
+                    pivot=mid
                     l=mid+1
                 else:
                     r=mid-1
-        return -1
-                
-        
-        
-           
-        
-            
-                
-            
+            return pivot
                     
-                
+        def find(l,r):
+            while l<=r:
+                mid=l+(r-l)//2
+                if nums[mid]==target:
+                    return mid
+                if nums[mid]<target:
+                    l=mid+1
+                else:
+                    r=mid-1
+            return -1
+            
+            
+        n=len(nums)
+        peak=findPeak(0,n-1)
+        bs1=find(0,peak)
+        if bs1!=-1:
+            return bs1
+        bs2=find(peak+1,n-1)
+        if bs2!=-1:
+            return bs2
+        return -1
         
