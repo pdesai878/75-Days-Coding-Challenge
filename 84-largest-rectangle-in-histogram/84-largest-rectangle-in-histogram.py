9649fc7e7b142e13543af213d5ff9809ce32654a
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-10-01 20:01:57 +0530

    Time: 3361 ms (5.01%), Space: 26.3 MB (99.81%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -1,56 +1,53 @@
 class Solution:
     def largestRectangleArea(self, arr: List[int]) -> int:
-        nsl,nsr=[],[]
-        stack=[]
         n=len(arr)
-        #nsl
+        nsl=[]
+        nsr=[]
+        stack=[]
         for i in range(n):
-            el=arr[i]
             if not stack:
                 nsl.append(-1)
-            elif stack and el>arr[stack[-1]]:
+            elif stack and arr[stack[-1]]<arr[i]:
                 nsl.append(stack[-1])
-            elif stack and el<=arr[stack[-1]]:
-                while stack and el<=arr[stack[-1]]:
+            elif stack and arr[stack[-1]]>=arr[i]:
+                while stack and arr[stack[-1]]>=arr[i]:
                     stack.pop()
                 if stack:
                     nsl.append(stack[-1])
                 else:
                     nsl.append(-1)
             stack.append(i)
+            
+        stack.clear()
         
-        #nsr
-        stack=[]
         for i in range(n-1,-1,-1):
-            el=arr[i]
             if not stack:
                 nsr.append(n)
-            elif stack and el>arr[stack[-1]]:
+            elif stack and arr[stack[-1]]<arr[i]:
                 nsr.append(stack[-1])
-            elif stack and el<=arr[stack[-1]]:
-                while stack and el<=arr[stack[-1]]:
+            elif stack and arr[stack[-1]]>=arr[i]:
+                while stack and arr[stack[-1]]>=arr[i]:
                     stack.pop()
                 if stack:
                     nsr.append(stack[-1])
                 else:
                     nsr.append(n)
+                    
             stack.append(i)
             
         nsr.reverse()
-      
-        mx=0
+ 
+        mx=-sys.maxsize
         for i in range(n):
-            mx=max(mx,(nsr[i]-nsl[i]-1)*arr[i])
+            width=nsr[i]-nsl[i]-1
+            area=arr[i]*width
+            mx=max(mx,area)
         return mx
-            
-        
         
                 
+       
             
-
+                    
+                    
             
-        
-        
-        
-                
         
