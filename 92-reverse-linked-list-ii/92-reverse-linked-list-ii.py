e76b0c811ef42ec243cde9daddccaf9493b767bb
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-10-01 17:56:54 +0530

    Time: 58 ms (38.26%), Space: 14.2 MB (18.47%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -5,48 +5,25 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        def reverse(p1,p2):
-            print(p1.val,p2.val)
-            curr=p1
-            prev=None
-            while prev!=p2:
-                nxt=curr.next
-                curr.next=prev
-                prev=curr
-                curr=nxt
-            return prev
+        if left==right:
+            return head
+        dummy=ListNode(-1)
+        dummy.next=head
+        prev=dummy
+
+        for i in range(left-1):
+            prev=prev.next
+            
+        tail=prev.next
+       
+        for _ in range(right-left):
+            temp=prev.next
+            prev.next=tail.next
+            tail.next=tail.next.next
+            prev.next.next=temp
+        return dummy.next
+            
         
-        count=1
-        prev=None
-        curr=head
-        while curr and count!=left:
-            prev=curr
-            curr=curr.next
-            count+=1
-        start=curr
-        while curr and count!=right:
-            curr=curr.next
-            count+=1
-        end=curr
-        nxt=curr.next
-        end.next=None
-        newHead=reverse(start,end)
-        
-        
-        
-        if prev:
-            prev.next=newHead
-        
-        curr=newHead
-        
-        while curr.next:
-            curr=curr.next
-        curr.next=nxt
-        
-        if left==1:
-            return newHead
-        
-        return head
         
       
         
