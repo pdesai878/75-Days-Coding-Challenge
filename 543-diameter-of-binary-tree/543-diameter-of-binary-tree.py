bcf4be7966cb066dde9ff8a855dd35b8b38a070c
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-10-01 14:47:56 +0530

    Time: 107 ms (14.24%), Space: 16.2 MB (84.83%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -8,15 +8,23 @@ class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         def postOrder(node):
             if not node:
-                return 0
+                return []
+            if not node.left and not node.right:
+                return [node.val]
+            
             left=postOrder(node.left)
             right=postOrder(node.right)
             
-            self.mx=max(self.mx,left+right)
+            length=len(left)+len(right)
+            if length>self.mx:
+                self.path=left+[node.val]+right
+                self.mx=length
             
-            return max(left,right)+1
+            return max(left,right,key=len)+[node.val]
         
         self.mx=-1
+        self.path=[]
         postOrder(root)
-        return self.mx
+        print(self.path)
+        return self.mx if self.path else 0
         
