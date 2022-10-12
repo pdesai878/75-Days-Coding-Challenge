60bf3729d43c29382ec2ede73b389fa7b6e61224
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-10-12 22:10:50 +0530

    Time: 51 ms (84.62%), Space: 14.1 MB (29.45%) - LeetHub

--- 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
@@ -1,30 +1,18 @@
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
-        n=len(arr)
-        m=len(pieces)
-        pieces.sort(key=lambda x:x[0])
+        dicti={p[0]:p for p in pieces}
         i=0
+        n=len(arr)
         while i<n:
-            left=0
-            right=m-1
-            found=-1
-            while left<=right:
-                mid=left+(right-left)//2
-                if pieces[mid][0]==arr[i]:
-                    found=mid
-                    for x in pieces[mid]:
-                        if x!=arr[i]:
-                            return False
-                        i+=1
-                    break
-                elif pieces[mid][0]>arr[i]:
-                    right=mid-1
-                else:
-                    left=mid+1
-            if found==-1:
+            if arr[i] in dicti:
+                for x in dicti[arr[i]]:
+                    if x!=arr[i]:
+                        return False
+                    i+=1
+            else:
                 return False
- 
         return True
+    
                     
         
       
