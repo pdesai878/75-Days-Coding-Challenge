992b0a8918fe6514d4d6a9e396fa216ff99695b4
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-10-22 15:30:50 +0530

    Time: 1700 ms (75.28%), Space: 15.4 MB (55.70%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -1,19 +1,24 @@
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
-        @lru_cache(None)
-        def getMin(i,j):   
+        def getMin(i,j):
             if i>j:
                 return 0
+            
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            
             mn=sys.maxsize
             for k in range(i,j+1):
-                curr_cost=cuts[j+1]-cuts[i-1]
-                mn=min(mn,curr_cost+getMin(i,k-1)+getMin(k+1,j))
+                cost=cuts[j+1]-cuts[i-1]
+                cost+=getMin(i,k-1)+getMin(k+1,j)
+                mn=min(mn,cost)
             
+            dp[i][j]=mn
             return mn
         
         N=len(cuts)
-        cuts.append(0)
-        cuts.append(n)
+        cuts=[0]+cuts+[n]
         cuts.sort()
+        dp=[[-1 for j in range(N+1)] for i in range(N+1)]
         return getMin(1,N)
-        
+            
