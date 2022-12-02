5546ceb3239b5d1f7cbf0a635e63333efba91a26
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-12-02 11:15:18 +0530

    Time: 104 ms (5.96%), Space: 14.1 MB (49.84%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,19 +1,15 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        def getSubSum(ind,s):
-            if ind>=n:
-                sub=tuple(sorted(s))
-                if sub not in res:
-                    res.add(sub)
-                return
-            
-            getSubSum(ind+1,s+[nums[ind]])
-            getSubSum(ind+1,s)
-            
         res=set()
         n=len(nums)
-        getSubSum(0,[])
-        return list(res)
-            
+        N=1<<n
         
+        for i in range(N):
+            sub=[]
+            for j in range(n):
+                if i&(1<<j):
+                    sub.append(nums[j])
+            res.add(tuple(sorted(sub)))
+        return res
+                
         
