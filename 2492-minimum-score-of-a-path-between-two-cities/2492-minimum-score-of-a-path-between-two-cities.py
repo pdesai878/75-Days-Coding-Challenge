31c66311cc93a792387bd47dd57380d2cf2a4b4e
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-12-08 21:42:54 +0530

    Time: 1722 ms (95.35%), Space: 78.9 MB (26.15%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,52 +1,19 @@
-class DSU:
-    def __init__(self,n):
-        self.n=n
-        self.parent=[i for i in range(n+1)]
-        self.rank=[0]*(n+1)
-    
-    def findParent(self,node):
-        if self.parent[node]==node:
-            return node
-        self.parent[node]=self.findParent(self.parent[node])
-        return self.parent[node]
-    
-    def union(self,u,v):
-        p1=self.findParent(u)
-        p2=self.findParent(v)
-        
-        if p1!=p2:
-            if self.rank[p1]>=self.rank[p2]:
-                self.parent[p2]=p1
-                self.rank[p1]+=self.rank[p2]
-            else:
-                self.parent[p1]=p2
-                self.rank[p2]+=self.rank[p1]
-                
-
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        dsu=DSU(n)
-       
+        def dfs(node):
+            visited[node]=True
+            mn=10**4+1
+            for neighbor,wt in adj[node].items():
+                mn=min(mn,wt)
+                if not visited[neighbor]:
+                    mn=min(mn,dfs(neighbor))
+            return mn
+        
+        adj=defaultdict(dict)
         for u,v,wt in roads:
-            dsu.union(u,v)   
+            adj[u][v]=wt
+            adj[v][u]=wt
         
-        self.mn=10**4+1
-        x=dsu.findParent(1)
-        for u,v,wt in roads:
-            p1=dsu.findParent(u)
-            p2=dsu.findParent(v)
-            
-            if p1==x or p2==x:
-                self.mn=min(self.mn,wt)
-        return self.mn
-                
-            
-        
-        
-        
-        
-        
-        
-        
-        
+        visited=[False for node in range(n+1)]
+        return dfs(1)
         
