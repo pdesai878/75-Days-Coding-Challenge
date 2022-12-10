55ce841236037e880907d2334f46fcacac961f71
Parth Desai <63565627+pdesai878@users.noreply.github.com>
2022-12-10 09:32:07 +0530

    Time: 919 ms (32.11%), Space: 75 MB (72.50%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.py
@@ -6,25 +6,22 @@
 #         self.right = right
 class Solution:
     def maxProduct(self, root: Optional[TreeNode]) -> int:
-        def getMax(node):
-            if not node:
-                return 0
-            left=getMax(node.left)
-            right=getMax(node.right)
-            curr=left+right+node.val
-            self.mx=max(self.mx,(total-curr)*curr)
-            return curr
-        
-        
         def getSum(node):
             if not node:
                 return 0
             left=getSum(node.left)
             right=getSum(node.right)
-            return left+right+node.val
+            s=left+right+node.val
+            sums.append(s)
+            return s
         
+        sums=[]
         total=getSum(root)
-        self.mx=-1
-        getMax(root)
-        return self.mx % (10 ** 9 + 7)
+        mx=-1
+        for s in sums:
+            mx=max(mx,s*(total-s))
+        return mx%(10**9+7)
+        
+        
+        
         
